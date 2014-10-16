016fc6d7b4136fa8f4edade8c43a696defedb92d
Phillip Bonner <pbonner@Phillips-Mac-mini.local>
2014-10-16 13:35:03 +1100

    Initial Commit

---
+++ Git Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Phillip Bonner on 16/10/2014.
+//  Copyright (c) 2014 Phillip Bonner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
