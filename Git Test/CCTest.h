482c3a5639045b35484aaefa91b1bc7addb0351f
Phillip Bonner <pbonner@Phillips-Mac-mini.local>
2014-10-16 13:56:48 +1100

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Phillip Bonner on 16/10/2014.
+//  Copyright (c) 2014 Phillip Bonner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
